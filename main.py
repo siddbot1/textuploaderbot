d720cc7a52dd4d263944595704c9167f62ed1b88
demo <61946705+kukkalu@users.noreply.github.com>
2023-06-02 16:19:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ async def account_login(bot: Client, m: Message):
         count = int(raw_text)
         
     max_concurrent_threads = 5
-    #count = 1
+    count = 1
     tasks = []
     
     async def process_link():
