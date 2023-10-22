2cf89b30901709a6454d1f9114dac18dd684d0da
siddbot1 <146437902+siddbot1@users.noreply.github.com>
2023-10-22 14:47:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-bot = Client(
-    "bot",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH"))import requests
+
 import json
 import subprocess
 from pyrogram.types.messages_and_media import message
