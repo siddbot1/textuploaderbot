9339482d38a104cfec523b8532521527b45a9333
siddbot1 <146437902+siddbot1@users.noreply.github.com>
2024-01-20 11:38:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ["API_ID", ""]
-    API_HASH = os.environ["API_HASH", ""]
-    AUTH_USERS= os.environ["AUTH_USERS", ""]
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6697904876:AAHV5THX4yTtAtnEY06tqjsTZZdQJ2nMwKY")
+    API_ID = int(os.environ["API_ID", "25218337"]
+    API_HASH = os.environ["API_HASH", "0984be1572879b9dabb10882fb46a539"]
+    AUTH_USERS= os.environ["AUTH_USERS", "5732659152"]
